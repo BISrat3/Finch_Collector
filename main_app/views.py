1b6ceccb9d01dcf7a20cb60085a27a1c032c69cf
bisrat <bisrat.amtataw@gmail.com>
2022-06-03 14:44:48 -0400

    add ratings

--- main_app/views.py
+++ main_app/views.py
@@ -27,36 +27,9 @@ class Home(TemplateView):
         # This is similar to response.send() in express
 
 class About(TemplateView):
-
-    # Here we are adding a method that will be ran when we are dealing with a GET request
-    # def get(self, request):
-        # Here we are returning a generic response
-        # This is similar to response.send() in express
-        # return HttpResponse("Finch About")
         template_name ="about.html"
 
-# class Finch:
-#     def __init__ (self, name, image, age):
-#         self.name = name 
-#         self.image = image
-#         self.age = age
-
-
-# finches = [
-#   Finch("House Finch", "https://www.allaboutbirds.org/guide/assets/photo/306327601-480px.jpg",
-#           "10 years"),
-#   Finch("Purple Finch",
-#           "https://cuteparrots.com/wp-content/uploads/2020/03/67284151-480px.jpg.webp", 
-#           "8 years"),
-#   Finch("Zebra Finch", "https://lafeber.com/pet-birds/wp-content/uploads/2018/06/Zebra-Finch.jpg",
-#           "9 years"),
-#   Finch("American Goldfinch",
-#           "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/306710541/1800",
-#            "6 years"),
-#   Finch("House Sparrow Finch",
-#           "https://upload.wikimedia.org/wikipedia/commons/6/6e/Passer_domesticus_male_%2815%29.jpg", " 5 years"),
-# ]
-
+    
 class FinchList(TemplateView):
     template_name = "finches_list.html"
 
@@ -105,10 +78,10 @@ class FinchDelete(DeleteView):
 class RatingCreate(View):
 
     def post(self, request, pk):
-        name = request.POST.get("name")
-        age = request.POST.get("age")
+        review = request.POST.get("review")
+        rating = request.POST.get("rating")
         finch = Finch.objects.get(pk=pk)
-        Rating.objects.create(name=name, age=age, finch=finch)
+        Rating.objects.create(review=review, rating=rating, finch=finch)
         return redirect('finches_detail', pk=pk)
 
 class BirdListFinchAssoc(View):
