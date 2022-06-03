2e2e7c3aa49496e38202eb4204fd293e40090a29
bisrat <bisrat.amtataw@gmail.com>
2022-06-02 21:50:14 -0400

    style_pages

--- main_app/models.py
+++ main_app/models.py
@@ -5,7 +5,7 @@ class Finch(models.Model):
 
     name = models.CharField(max_length=100)
     img = models.CharField(max_length=250)
-    age = models.TextField(max_length=100)
+    age = models.CharField(max_length=100)
     
     def __str__(self):
         return self.name
